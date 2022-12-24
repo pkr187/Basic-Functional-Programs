5fc8b907b4bc43a01756728157c550c2a194ae5d
pkr187 <pankajroman187@gmail.com>
2022-12-24 20:22:17 +0530

    [pankaj] Add:FlipCoins

---
+++ BasicFunctionalProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BasicFunctionalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select program\n 1.flip coin");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoins flipCoins = new FlipCoins();
+                    flipCoins.Coin();
+                    break;
+            }
+        }
+    }
+}
