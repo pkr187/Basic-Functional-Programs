775f2bc11dafc79ff5c627efc6cae1a5714b68b8
pkr187 <pankajroman187@gmail.com>
2022-12-24 21:00:31 +0530

    [pankaj] Add:Powerof2

--- BasicFunctionalProgram/Program.cs
+++ BasicFunctionalProgram/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Select program\n 1.LeapYear");
+            Console.WriteLine("Select program\n 1.PowerOf2");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
-                    LeapYear leapYear  = new LeapYear();
-                    leapYear.Year();
+                    PowerOf2 powerOf2  = new PowerOf2();
+                    powerOf2.Read();
                     break;
             }
         }
