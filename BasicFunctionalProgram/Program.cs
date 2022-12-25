ce125caca5c74fc8046c20402d668916d85fd920
pkr187 <pankajroman187@gmail.com>
2022-12-25 22:12:32 +0530

    [pankaj] Add:QuotientRemainder

--- BasicFunctionalProgram/Program.cs
+++ BasicFunctionalProgram/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Select program\n 1.SwapNumber");
+            Console.WriteLine("Select program\n 1.QuotientRemainder");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
-                    SwapNumber swapNumbers = new SwapNumber();
-                    swapNumbers.Swap();
+                    QuotientRemainder quotientRemainder = new QuotientRemainder();
+                    quotientRemainder.QuoRem();
                     break;
             }
         }
---
+++ BasicFunctionalProgram/QuotientRemainder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFunctionalProgram
+{
+    internal class QuotientRemainder
+    {
+        int dividend = 19, divisor = 5;
+        public void QuoRem()
+        {
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+            Console.WriteLine("quotient = " + quotient);
+            Console.WriteLine("remainder = " + remainder);
+        }
+    }
+}
+    
+
